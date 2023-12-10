e40ef55a174484ca687e1bf4c3acd42c8d99d9c8
River <dbrkfka781@gmail.com>
2023-12-11 02:53:09 +0900

    Initial Commit

---
+++ github_test/github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testApp.swift
+//  github_test
+//
+//  Created by 유가람 on 2023/12/11.
+//
+
+import SwiftUI
+
+@main
+struct github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
